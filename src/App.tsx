81c585333351d375ec313079024656c163485881
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2024-05-29 16:00:51 -0300

    fix: typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex mobile-h-screen w-screen items-center justify-center bg-gradient-to-tr from-orange-600 to bg-orange-400">
       <div className="bg-amber-900 text-white rounded-md p-40 flex items-center justify-center shadow-lg border-2 border-orange-800 text-2xl font-thin animate-pulse hover:scale-90 duration-300 ease-in-out cursor-wait">
-        <p> Homme Coffee Incoming...</p>
+        <p> Home Coffee Incoming...</p>
       </div>
     </div>
   );
