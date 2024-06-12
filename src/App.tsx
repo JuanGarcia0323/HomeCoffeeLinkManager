849580be8761ed51167ca655a2dbd74877ea2641
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2024-06-12 12:42:46 -0300

    better looking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="flex mobile-h-screen w-screen items-center justify-center bg-gradient-to-tr from-orange-600 to bg-orange-400">
-      <div className="bg-amber-900 text-white rounded-md p-40 flex items-center justify-center shadow-lg border-2 border-orange-800 text-2xl font-thin animate-pulse hover:scale-90 duration-300 ease-in-out cursor-wait">
-        <p> Home Coffee Incoming...</p>
+      <div className="bg-amber-900 text-white rounded-md w-3/4 lg:w-2/4 h-1/4 lg:h-3/4 p-8 flex items-center justify-center shadow-lg border-2 border-orange-800 text-xl lg:text-2xl font-thin animate-pulse hover:scale-90 duration-300 ease-in-out cursor-wait">
+        <p className="text-center"> Home Coffee Incoming...</p>
       </div>
     </div>
   );
